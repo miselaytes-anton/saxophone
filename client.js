61e71a88e4d634b0dd4c56ad058741754834021f
Anton Miselaytes <anton.miselaytes@philips.com>
2016-04-24 14:00:31 +0200

    x

--- client.js
+++ client.js
@@ -43,9 +43,9 @@ reconnect(function(conn){
     remoteEmitter.on('numCallersChange', function(numCallers){
         if (phone.isOn){
             var videoIndex = numCallers - 1;
-            video.play(videoIndex);
+            if ( !opts.isTestMode ) video.play(videoIndex);
         } else {
-            video.quit();
+            if ( !opts.isTestMode ) video.quit();
         }
         console.log('> switching video, because num changed to %s', numCallers);
         console.log('phone is ', phone.isOn? 'on': 'off')
